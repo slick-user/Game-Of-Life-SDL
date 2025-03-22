e9b0268e52c936ab78c369d23e19a14e2a8159c7
Slick User <azyaz005@gmail.com>
2025-03-23 00:54:18 +0500

    Game of Life is broken it does not work (sigh....)

--- Editor.h
+++ Editor.h
@@ -62,40 +62,66 @@ void Editor::deleteCell() {
 
 void Editor::step() {
 
-  // not a data type I have used before but is an interesting thing..
   std::vector<std::pair<int, int>> neighbourOffsets = {
     {-50, -50}, {0, -50}, {50, -50},
     {-50, 0  },           {50,  0 }, 
     {-50, 50 }, {0,  50}, {50, 50 }
   };
 
+  std::vector<std::pair<int, int>> deadCells; // x and y coords of dead cells
+  std::vector<int> deadCellNeighbour; // neighbour count for dead cells
   std::vector<int> toRemove; 
 
   for (int i=0; i<int(cell.size()); i++) { 
     int neighbours = 0;
 
-    for (int j=0; j<int(cell.size()); j++) {
-      if (j == i) continue;
-      
-      for (auto [dx, dy] : neighbourOffsets) {
-        if (cell[i].cell.x + dx == cell[j].cell.x && cell[i].cell.y + dy == cell[j].cell.y) { 
+    bool found = false;
+    for (auto [dx, dy] : neighbourOffsets) {
+      int nx = cell[i].cell.x + dx;
+      int ny = cell[i].cell.y + dy;
+  
+      // This is for the live cells to check their neighbours
+      for (int j=0; j < int(cell.size()); j++) {
+        if (cell[j].getX() == nx && cell[j].getY() == ny) { 
           neighbours++;
+          found = true;
+          break;
+        }
+      }
+      if (!found && neighbours == 0) {
+        bool alreadyTracked = false;
+        for (int k=0; k < int(deadCells.size()); k++) {
+          if (deadCells[k].first == nx && deadCells[k].second == ny) {
+            deadCellNeighbour[k]++;
+            alreadyTracked = true;
+            break;
+          }
+        }
+        if (!alreadyTracked) {
+          deadCells.push_back({nx, ny});
+          deadCellNeighbour.push_back(1);    
         }
       }
     }
 
-    std::cout << "CELL: " << i << " has " << neighbours << " neighbours\n";
-
-    if (neighbours < 2)
+    if (neighbours < 2 || neighbours > 3)
       toRemove.push_back(i); 
-  
+   
   }
 
+  // Revive the cells that are to live
+  for (int i=0; i < int(deadCells.size()); i++) {
+    if (deadCellNeighbour[i] == 3) {
+      cell.push_back(Cell(deadCells[i].first, deadCells[i].second));
+    }
+  } 
+
+  // Remove the cells that are to die
   for (int i = int(toRemove.size()) - 1; i >= 0; i--) {
-    if (toRemove[i] >= 0 && toRemove[i] < int(cell.size()) ) 
+    if (toRemove[i] >= 0 && toRemove[i] < int(cell.size()) ) { 
       cell.erase(cell.begin() + toRemove[i]);
+    }
   }
-
 }
 
 // now we check 8 dimenstions of neighbours, and we do so iteratively and we make the changes after going through the entire array of cells instead of while iterating
